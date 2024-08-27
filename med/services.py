298d9f18b56609bd5f34e6820abac7950a6ea5b4
Сергей Арефьев <Simpson6515@yandex.ru>
2024-08-27 23:45:38 +0500

    tenth commit

--- med/services.py
+++ med/services.py
@@ -4,7 +4,19 @@ from django.core.mail import send_mail
 
 
 class StileFormMixin:
-    """Формы"""
+    """
+    A mixin class for Django forms that adds a common style to all form fields.
+
+    Attributes:
+    -----------
+    fields : dict
+        A dictionary containing the form fields.
+
+    Methods:
+    --------
+    __init__(self, *args, **kwargs)
+        Initializes the form with the provided arguments and applies a common style to all form fields.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,6 +26,21 @@ class StileFormMixin:
 
 
 def send_new_password(email, new_password):
+    """
+    Sends a new password to the specified email address.
+
+    Parameters:
+    -----------
+    email : str
+        The email address to which the new password will be sent.
+    new_password : str
+        The new password to be sent to the email address.
+
+    Returns:
+    --------
+    None
+        This function does not return any value. It sends an email with the new password.
+    """
     send_mail(
         subject='Пароль изменен',
         message=f'Новый пароль {new_password}',
